6d97ac8391823ef1cde65608ff3c4d76b5079697
clonidine <lcly@duck.com>
2024-04-25 17:05:00 -0300

    add vscodium user settings

--- pkgs/vscodium/default.nix
+++ pkgs/vscodium/default.nix
@@ -2,6 +2,14 @@
    programs.vscode = {
      enable = true;
      package = pkgs.vscodium;
+     userSettings = {
+	"files.autoSave" = "onFocusChange";
+	"editor.fontSize" = 16;
+        "editor.fontFamily" = "'JetBrainsMono Nerd Font'";
+	"editor.cursorStyle" = "block";
+	"editor.fontLigatures" = true;
+	"workbench.colorTheme" = "Catppuccin Mocha";
+     };
      extensions = with pkgs.vscode-extensions; [
        rust-lang.rust-analyzer
        tamasfe.even-better-toml
