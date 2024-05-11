41e7e199fd1d7d68c7a67d2337da4b4184d6ac31
clonidine <lcly@duck.com>
2024-05-11 16:03:05 -0300

    chore: add github theme

--- pkgs/alacritty/default.nix
+++ pkgs/alacritty/default.nix
@@ -2,7 +2,11 @@
 
 {
   imports = [
+    # Themes
+    #    ./themes/catppuccin/mocha.nix
+    #    ./themes/github/github_dark_high_contrast.nix
     ./themes/midnight-haze.nix
+
     ./font.nix
   ];
 
