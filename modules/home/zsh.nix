eb82f89a76416cff7cad54ee9af1f75a860c3853
clonidine <lcly@duck.com>
2025-03-25 11:35:04 -0300

    zsh: add zsh-autosuggestions and git plugin, add aliases

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -1,9 +1,45 @@
 { pkgs, ... }:
 
+let
+  myAliases = {
+    # Git
+    g = "git";
+    ga = "git add";
+    gs = "git status";
+    gl = "git log";
+    gd = "git diff";
+    gc = "git commit";
+    gp = "git push";
+
+    # Others
+    ls = "eza";
+    cat = "bat";
+    hrbd = "home-manager switch --flake .#mika";
+    rbd = "sudo nixos-rebuild switch --flake .#mika";
+  };
+in
 {
   programs.zsh = {
     enable = true;
-    plugins = with pkgs; [ zsh-autosuggestions ];
-    dotDir = ".config/zsh";
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        # {
+        #   name = "romkatv/powerlevel10k";
+        #   tags = [
+        #     "as:theme"
+        #     "depth:1"
+        #   ];
+        # }
+      ];
+    };
+    enableCompletion = true;
+    shellAliases = myAliases;
   };
 }
