f938bd7f51ad4051254f4b874afc45bd5b9d65f7
clonidine <lcly@duck.com>
2025-11-18 15:07:24 -0300

    add freetube and configure wireplumber default audio volume

--- hosts/mika/modules-imports.nix
+++ hosts/mika/modules-imports.nix
@@ -13,6 +13,7 @@
     ../../modules/home/firefox.nix
     ../../modules/home/direnv.nix
     ../../modules/home/nvim.nix
+    ../../modules/home/wireplumber.nix
 
     ../../modules/desktop/gnome.nix
     ../../modules/desktop/hyprland.nix
