d908a8759fcb652f89aea17cac50c235067c44f7
clonidine <lcly@tuta.io>
2026-02-14 14:39:48 -0300

    feat: enable declarative ssh signing

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,12 +1,34 @@
 { pkgs, ... }:
 
+let
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5ipYjy2mBU8zthIVQTVX9wUHw3jutXJGqVxRSg/Gnw";
+  email = "lcly@tuta.io";
+in
 {
+  home.file.".ssh/allowed_signers".text = ''
+    ${email} ${publicKey}
+  '';
+
+  home.file.".ssh/id_ed25519.pub".text = publicKey;
+
   programs.git = {
     enable = true;
+
     settings = {
-    	user.name = "clonidine";
-    	user.email = "lcly@duck.com";
-        init.defaultBranch = "main";
+      user = {
+        name = "clonidine";
+        email = email;
+        signingkey = "~/.ssh/id_ed25519.pub";
+      };
+
+      gpg.format = "ssh";
+
+      "gpg \"ssh\"".program = "${pkgs.openssh}/bin/ssh-keygen";
+
+      "gpg \"ssh\"".allowedSignersFile = "~/.ssh/allowed_signers";
+
+      commit.gpgsign = true;
+      tag.gpgsign = true;
     };
   };
 }
