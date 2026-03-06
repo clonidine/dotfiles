804dc04741f6bdd7d973633fc9b0ba7354ec9a52
clonidine <86500701+clonidine@users.noreply.github.com>
2026-03-06 06:17:30 -0300

    harden nextcloud secrets workflow and add local secret helper

--- secrets.nix
+++ secrets.nix
@@ -7,4 +7,19 @@ in
     mika
     desktop
   ];
+
+  "secrets/mika-password-hash.age".publicKeys = [
+    mika
+    desktop
+  ];
+
+  "secrets/nextcloud-admin-pass.age".publicKeys = [
+    mika
+    desktop
+  ];
+
+  "secrets/nextcloud-mika-pass.age".publicKeys = [
+    mika
+    desktop
+  ];
 }
