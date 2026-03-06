4d133966a2d7f887a1fadb8da876ad7171b2e831
clonidine <86500701+clonidine@users.noreply.github.com>
2026-03-06 05:43:21 -0300

    add nix-darwin host configuration for macOS

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -35,6 +40,7 @@
       self,
       nixpkgs,
       home-manager,
+      darwin,
       agenix,
       spicetify-nix,
       lunar-client,
@@ -88,14 +94,32 @@
           };
         };
 
+      mkDarwin =
+        host: system:
+        darwin.lib.darwinSystem {
+          inherit system;
+          modules = [
+            ./hosts/${host}/darwin.nix
+            home-manager.darwinModules.home-manager
+          ];
+          specialArgs = {
+            inherit spicetify-nix inputs;
+          };
+        };
+
     in
     {
       nixosConfigurations = {
         mika = mkNixos "mika" "x86_64-linux";
       };
 
+      darwinConfigurations = {
+        mika-macbook = mkDarwin "mika-macbook" "aarch64-darwin";
+      };
+
       homeConfigurations = {
         mika = mkHome "mika" "x86_64-linux";
+        "mika@macbook" = mkHome "mika-macbook" "aarch64-darwin";
       };
 
       packages = forAllSystems (
